630758188c993b06d555962cc59cdad7363d9f15
tutn <tu.nguyenthanh@axonactive.com>
2024-05-22 17:26:11 +0700

    Update structure

---
+++ karma.conf.js
@@ -0,0 +1,60 @@
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
+
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-webpack'),
+      require('karma-chrome-launcher'),
+      require("karma-jsdom-launcher"),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-junit-reporter')
+    ],
+    client: {
+      jasmine: {
+        // you can add configuration options for Jasmine here
+        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+        // for example, you can disable the random execution with `random: false`
+        // or set a specific seed with `seed: 4321`
+        stopSpecOnExpectationFailure: true,
+        failFast: true,
+        timeoutInterval: 60000
+      },
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true // removes the duplicated traces
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/marketplace-ui'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' }
+      ]
+    },
+    angularCli: {
+      environment: 'dev'
+    },
+    reporters: ['progress', 'junit'],
+    junitReporter: {
+      outputDir: 'reports',
+      outputFile: 'karma-junit.xml',
+      useBrowserName: false
+    },
+    browsers: ['ChromeHeadless'],
+    restartOnFileChange: true,
+    customLaunchers: {
+      Chrome_with_debugging: {
+        base: 'Chrome',
+        flags: ['--remote-debugging-port=9222'],
+        debug: true
+      }
+    },
+  });
+};
