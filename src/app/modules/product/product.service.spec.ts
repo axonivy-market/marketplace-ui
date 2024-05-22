671b527f5a925d4f98e1552da8f37e5766a8b982
tutn <tu.nguyenthanh@axonactive.com>
2024-05-22 17:32:56 +0700

    Update unit test

--- src/app/modules/product/product.service.spec.ts
+++ src/app/modules/product/product.service.spec.ts
@@ -3,6 +3,9 @@ import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ProductService } from './product.service';
 
+const PRODUCT_ID = 'adobe-acrobat-connector';
+const NOT_EXIST_ID = 'undefined';
+
 describe('ProductService', () => {
   let service: ProductService;
 
@@ -16,5 +19,23 @@ describe('ProductService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('getAllProducts should return list of product', () => {
+    service.getAllProducts().subscribe((products) => {
+      expect(products.length).toBeGreaterThan(0);
+    });
+  });
+
+  it('getProductById should return a product', () => {
+    service.getProductById(PRODUCT_ID).subscribe((data) => {
+      expect(data.id).toEqual(PRODUCT_ID);
+    });
+  });
+
+  it('getProductById should return null product', () => {
+    service.getProductById(NOT_EXIST_ID).subscribe((data) => {
+      expect(data).toEqual({} as any);
+    });
   });
 });
