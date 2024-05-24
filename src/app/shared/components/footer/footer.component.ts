2b118baa747f843d87628e8eda32e295f327375c
tutn <tu.nguyenthanh@axonactive.com>
2024-05-24 10:47:11 +0700

    Update assets folder

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -3,13 +3,14 @@ import { Subscription } from 'rxjs';
 import { ThemeService } from '../../../core/services/theme/theme.service';
 import { NavItem } from '../../models/nav-item.model';
 import { NAV_ITEMS } from '../../constants/common.constant';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss',
+  styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
   isDarkMode!: boolean;
