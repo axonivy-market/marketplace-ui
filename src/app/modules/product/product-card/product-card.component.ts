641a914d8f85b76b465a637e2d19201cac756ba0
tutn <tu.nguyenthanh@axonactive.com>
2024-05-28 16:13:40 +0700

    remove unused code

--- src/app/modules/product/product-card/product-card.component.ts
+++ src/app/modules/product/product-card/product-card.component.ts
@@ -1,15 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input, inject } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 import { ThemeService } from '../../../core/services/theme/theme.service';
 import { Product } from '../../../shared/models/product.model';
 import { ProductLogoPipe } from '../../../shared/pipes/logo.pipe';
-import { CommonModule } from '@angular/common';
-import { TranslateModule } from '@ngx-translate/core';
-import { ProductTypePipe } from '../../../shared/pipes/product-type.pipe';
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [CommonModule, ProductLogoPipe, ProductTypePipe, TranslateModule],
+  imports: [CommonModule, ProductLogoPipe, TranslateModule],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.scss'
 })
