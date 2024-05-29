529c3a78cee624b8993f5fba58f5727f4746a305
tutn <tu.nguyenthanh@axonactive.com>
2024-05-29 16:24:47 +0700

    Update footer

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -4,19 +4,16 @@ import { ThemeService } from '../../../core/services/theme/theme.service';
 import { NavItem } from '../../models/nav-item.model';
 import { NAV_ITEMS } from '../../constants/common.constant';
 import { TranslateModule } from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-  isDarkMode!: boolean;
-
-  subscriptions: Subscription[] = [];
-
   themeService = inject(ThemeService);
 
   navItems: NavItem[] = NAV_ITEMS;
