88a480e63b30ee9e2ac3d76133c345099cd43df7
Dinh Nguyen <dinh.nguyentrinhquoc@axonactive.com>
2024-06-04 11:04:04 +0700

    update ui test

--- src/app/shared/components/header/header.component.spec.ts
+++ src/app/shared/components/header/header.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HeaderComponent } from './header.component';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { of } from 'rxjs';
+import { By } from '@angular/platform-browser';
 
 class TranslateServiceStub {
   get(key: any): any {
@@ -23,7 +24,7 @@ describe('HeaderComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [HeaderComponent, TranslateModule.forRoot()],
-      providers: [TranslateService],
+      providers: [TranslateService]
     }).compileComponents();
 
     fixture = TestBed.createComponent(HeaderComponent);
@@ -39,5 +40,64 @@ describe('HeaderComponent', () => {
     spyOn(component.translateService, 'use').and.stub();
     component.onSelectLanguage('en');
     expect(component.translateService.use).toHaveBeenCalled();
+  });
+
+  it('should toggle the search input visibility on search icon click', () => {
+    const searchIcon = fixture.debugElement.query(
+      By.css('.header__search-button i')
+    );
+    const searchInput = fixture.debugElement.query(
+      By.css('.header__search-input')
+    );
+
+    expect(component.isSearchBarDisplayed).toBeFalse();
+    expect(searchInput.attributes['hidden']).toBeDefined();
+
+    // Click the search icon
+    searchIcon.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    expect(component.isSearchBarDisplayed).toBeTrue();
+    expect(searchInput.attributes['hidden']).toBeUndefined();
+
+    // Click the search icon again
+    searchIcon.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    expect(component.isSearchBarDisplayed).toBeFalse();
+    expect(searchInput.attributes['hidden']).toBeDefined();
+  });
+
+  it('should toggle the mobile menu on click', () => {
+    const navbarToggler = fixture.debugElement.query(
+      By.css('.navbar-toggler span')
+    );
+
+    expect(component.isMobileMenuCollapsed).toBeTrue();
+
+    // Click the mobile menu toggler
+    navbarToggler.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    expect(component.isMobileMenuCollapsed).toBeFalse();
+
+    // Click the mobile menu toggler again
+    navbarToggler.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    expect(component.isMobileMenuCollapsed).toBeTrue();
+  });
+
+  it('should toggle the theme on theme button click', () => {
+    spyOn(component.themeService, 'changeTheme').and.callThrough();
+    const themeButton = fixture.debugElement.query(
+      By.css('.header__theme-button div')
+    );
+
+    // Click the theme button
+    themeButton.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    expect(component.themeService.changeTheme).toHaveBeenCalled();
   });
 });
