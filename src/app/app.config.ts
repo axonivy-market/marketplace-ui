bfdc04e4dd03f3259b48f36672e7ddc1e317d91f
tutn <tu.nguyenthanh@axonactive.com>
2024-06-06 11:51:33 +0700

    Update app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,28 +1,19 @@
-import { HttpClient, provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
-import { ApplicationConfig, importProvidersFrom, provideExperimentalZonelessChangeDetection } from '@angular/core';
+import {
+  HttpClient,
+  provideHttpClient,
+  withFetch,
+  withInterceptors
+} from '@angular/common/http';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideExperimentalZonelessChangeDetection
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { Observable, map } from 'rxjs';
-import { parse } from 'yaml';
 import { routes } from './app.routes';
+import { HttpLoaderFactory } from './core/configs/translate.config';
 import { apiInterceptor } from './core/interceptors/api.interceptor';
-
-class TranslateYamlHttpLoader implements TranslateLoader {
-  constructor(
-    private readonly http: HttpClient,
-    public path: string = '/assets/i18n/',
-  ) {}
-
-  public getTranslation(lang: string): Observable<Object> {
-    return this.http
-      .get(`${this.path}${lang}.yaml`, { responseType: 'text' })
-      .pipe(map((data) => parse(data)));
-  }
-}
-
-export function HttpLoaderFactory(httpClient: HttpClient) {
-  return new TranslateYamlHttpLoader(httpClient);
-}
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -34,9 +25,9 @@ export const appConfig: ApplicationConfig = {
         loader: {
           provide: TranslateLoader,
           useFactory: HttpLoaderFactory,
-          deps: [HttpClient],
-        },
-      }),
-    ),
-  ],
+          deps: [HttpClient]
+        }
+      })
+    )
+  ]
 };
