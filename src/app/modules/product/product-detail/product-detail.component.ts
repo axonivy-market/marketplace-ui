d398208e07dbeb54b7010b388d413e4f51b66eac
Khanh Nguyen <khanh.nguyenduy@axonactive.com>
2024-06-07 11:50:09 +0700

    MARP-357: Implement UI for comment panel

--- src/app/modules/product/product-detail/product-detail.component.ts
+++ src/app/modules/product/product-detail/product-detail.component.ts
@@ -2,20 +2,26 @@ import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from '../../../shared/models/product.model';
 import { ProductService } from '../product.service';
+import { ProductCommentComponent } from '../product-comment/product-comment.component';
+import { COMMENTS } from '../../../shared/constants/common.constant';
+import { ThemeService } from '../../../core/services/theme/theme.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-detail',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ProductCommentComponent],
   providers: [ProductService],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.scss',
 })
 export class ProductDetailComponent {
   product!: Product;
+  comments: Object[] = COMMENTS;
 
   route = inject(ActivatedRoute);
   productService = inject(ProductService);
+  themeService = inject(ThemeService);
 
   constructor() {
     const productId = this.route.snapshot.params['id'];
