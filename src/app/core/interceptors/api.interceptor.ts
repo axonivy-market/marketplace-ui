163cc63cf5ec3eb0a94739cc99e36e1d69b9ad03
Hoan Nguyen <nqhoan@axonactive.com>
2024-06-11 02:06:19 +0700

    fix debug call to api

--- src/app/core/interceptors/api.interceptor.ts
+++ src/app/core/interceptors/api.interceptor.ts
@@ -1,11 +1,17 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpHeaders, HttpInterceptorFn } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 
 export const apiInterceptor: HttpInterceptorFn = (req, next) => {
   if (req.url.includes('i18n')) {
     return next(req);
   }
-
-  const cloneReq = req.clone({ url: `${environment.apiUrl}/${req.url}` });
+  const cloneReq = req.clone({ url: `${environment.apiUrl}/${req.url}`, headers: addIvyHeaders(req.headers) });
   return next(cloneReq);
 };
+
+function addIvyHeaders(headers: HttpHeaders): HttpHeaders {
+  if (headers.has("X-Requested-By")) {
+    return headers;
+  }
+  return headers.append("X-Requested-By", "ivy");
+}
