7cbe3208052c47f1f62e562f87396d859682be78
Dinh Nguyen <dinh.nguyentrinhquoc@axonactive.com>
2024-06-11 11:17:41 +0700

    update UI

--- src/app/modules/product/product-detail/product-version-action/product-version-action.component.ts
+++ src/app/modules/product/product-detail/product-version-action/product-version-action.component.ts
@@ -1,10 +1,9 @@
-import { Component, AfterViewInit, inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ThemeService } from '../../../../core/services/theme/theme.service';
 import { TranslateModule } from '@ngx-translate/core';
 import { CommonModule } from '@angular/common';
 import { TranslateService } from '@ngx-translate/core';
 import { FormsModule } from '@angular/forms';
-import * as bootstrap from 'bootstrap';
 @Component({
   selector: 'app-product-version-action',
   standalone: true,
@@ -13,14 +12,6 @@ import * as bootstrap from 'bootstrap';
   styleUrl: './product-version-action.component.scss'
 })
 export class ProductVersionActionComponent {
-  ngAfterViewInit() {
-    const dropdownElementList = [].slice.call(
-      document.querySelectorAll('.dropdown-toggle')
-    );
-    dropdownElementList.map(function (dropdownToggleEl) {
-      return new bootstrap.Dropdown(dropdownToggleEl);
-    });
-  }
   versions: string[] = ['10', '11'];
   artifacts: String[] = ['portal', 'portal app'];
   themeService = inject(ThemeService);
