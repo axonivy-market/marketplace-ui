4b7aaea4a6d43c8935c25bfc7f5d60ae377caada
Dinh Nguyen <dinh.nguyentrinhquoc@axonactive.com>
2024-06-11 15:20:47 +0700

    update service

--- src/app/modules/product/product-detail/product-version-action/product-version-action.component.ts
+++ src/app/modules/product/product-detail/product-version-action/product-version-action.component.ts
@@ -5,6 +5,11 @@ import { CommonModule } from '@angular/common';
 import { TranslateService } from '@ngx-translate/core';
 import { FormsModule } from '@angular/forms';
 import { ProductService } from '../../product.service';
+import {
+  VersionData,
+  Artifact
+} from '../../../../shared/models/vesion-artifact.model';
+import { log } from 'console';
 @Component({
   selector: 'app-product-version-action',
   standalone: true,
@@ -16,28 +21,32 @@ export class ProductVersionActionComponent {
   @Input()
   productId!: string;
   versions: string[] = ['10', '11'];
-  artifacts: String[] = ['portal', 'portal app'];
+  artifacts: Artifact[] = [];
   themeService = inject(ThemeService);
   translateService = inject(TranslateService);
   isDevVersionsDisplayed = false;
   designerVersion: string = '';
-  selectedArtifact: string = '10';
+  selectedArtifact!: Artifact;
   selectedVersion: string = 'portal';
   productService = inject(ProductService);
 
   onShowVersionAndArtifact() {
-    this.versions = [];
-    this.artifacts = [];
-    let a = this.productService.sendRequestToProductDetailVersionAPI(
-      this.productId,
-      this.isDevVersionsDisplayed,
-      this.designerVersion
-    );
-    console.log(a);
+    console.log('asd');
+    this.productService
+      .sendRequestToProductDetailVersionAPITest(this.productId, false, '')
+      .subscribe((data: VersionData) => {
+        console.log(data);
+
+        this.versions = Object.keys(data);
+        if (this.versions.length > 0) {
+          this.selectedVersion = this.versions[0];
+          this.artifacts = data[this.selectedVersion];
+          this.selectedArtifact = this.artifacts[0];
+        }
+      });
   }
 
   downloadArifact() {
-    console.log(this.selectedArtifact);
-    console.log(this.selectedVersion);
+    window.open(this.selectedArtifact.downloadUrl, '_blank');
   }
 }
--- src/app/modules/product/product.service.ts
+++ src/app/modules/product/product.service.ts
@@ -6,39 +6,44 @@ import { SortType } from '../../shared/enums/sort-type.enum';
 import { MOCK_PRODUCTS } from '../../shared/mocks/mock-data';
 import { Criteria } from '../../shared/models/criteria.model';
 import { Product } from '../../shared/models/product.model';
-import { error, log } from 'console';
-
+import { VersionData } from '../../shared/models/vesion-artifact.model';
 @Injectable()
 export class ProductService {
   httpClient = inject(HttpClient);
 
   sendRequestToProductAPI(url: string, products: Product[]) {
-    firstValueFrom(this.httpClient.get(url)).then((response: any) => {
-      if (response) {
-        console.log("Content " + response._embedded.products);
-        console.log("Links " + response._links.next);
-        console.log("Page " + response.page);
-      }
-      if (products === undefined) {
-        products = response.content;
-      } else {
-        products = products.concat(response.content);
-      }
-      let nextLink = response._links.next;
+    firstValueFrom(this.httpClient.get(url))
+      .then((response: any) => {
+        if (response) {
+          console.log('Content ' + response._embedded.products);
+          console.log('Links ' + response._links.next);
+          console.log('Page ' + response.page);
+        }
+        if (products === undefined) {
+          products = response.content;
+        } else {
+          products = products.concat(response.content);
+        }
+        let nextLink = response._links.next;
 
-      if (nextLink != '') {
-        this.sendRequestToProductAPI(nextLink.href, products);
-      } else {
-        console.log('Finished');
-      }
-    }).catch(err => {
-      console.log(err.error.errorCode + err.error.message, err.error.helpText);
-    }).finally(() => console.log('Finish call'));
+        if (nextLink != '') {
+          this.sendRequestToProductAPI(nextLink.href, products);
+        } else {
+          console.log('Finished');
+        }
+      })
+      .catch(err => {
+        console.log(
+          err.error.errorCode + err.error.message,
+          err.error.helpText
+        );
+      })
+      .finally(() => console.log('Finish call'));
   }
 
   getAllProducts(criteria: Criteria) {
     let dummyProduct: Product[] = [];
-    this.sendRequestToProductAPI("api/product/" + criteria.type, dummyProduct);
+    this.sendRequestToProductAPI('api/product/' + criteria.type, dummyProduct);
   }
 
   getProductById(productId: string): Observable<Product> {
@@ -111,7 +116,7 @@ export class ProductService {
   }
 
   sendRequestToProductDetailVersionAPI(
-    prductId: string,
+    productId: string,
     showDevVersion: boolean,
     designerVersion: string
   ) {
@@ -120,8 +125,8 @@ export class ProductService {
       .append('showDevVersion', showDevVersion);
 
     const headers = new HttpHeaders();
-    let url = prductId + '/versions';
-    firstValueFrom(
+    let url = 'api/product-details/' + productId + '/versions';
+    return firstValueFrom(
       this.httpClient.get(url, { headers: headers, params: params })
     )
       .then((response: any) => {
@@ -129,5 +134,17 @@ export class ProductService {
       })
       .catch()
       .finally();
+  }
+
+  sendRequestToProductDetailVersionAPITest(
+    productId: string,
+    showDevVersion: boolean,
+    designerVersion: string
+  ): Observable<VersionData> {
+    let url = 'api/product-details/' + productId + '/versions';
+    const params = new HttpParams()
+      .append('designerVersion', designerVersion)
+      .append('showDevVersion', showDevVersion);
+    return this.httpClient.get<VersionData>(url, { params: params });
   }
 }
