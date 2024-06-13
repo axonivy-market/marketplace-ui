04e7feb9d7c867914889f283d37bb4a83aa39be9
PHAM HOANG HUNG <hung.pham@axonactive.com>
2024-06-13 09:48:58 +0700

    init login page with github account

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,17 @@
 import { Routes } from '@angular/router';
+import { GitAuthComponent } from './modules/git-auth/git-auth.component';
 
 export const routes: Routes = [
   {
     path: '',
-    loadChildren: () =>
-      import('./modules/home/home.routes').then((m) => m.routes),
+    loadChildren: () => import('./modules/home/home.routes').then(m => m.routes)
   },
   {
     path: ':id',
     loadChildren: () =>
-      import('./modules/product/product.routes').then((m) => m.routes),
+      import('./modules/product/product.routes').then(m => m.routes)
   },
+  {
+    path: 'auth/callback', component: GitAuthComponent
+  }
 ];
--- src/app/core/interceptors/api.interceptor.ts
+++ src/app/core/interceptors/api.interceptor.ts
@@ -1,11 +1,33 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpHeaders, HttpInterceptorFn } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
+
+export const REQUEST_BY = 'X-Requested-By';
+export const IVY = 'ivy';
 
 export const apiInterceptor: HttpInterceptorFn = (req, next) => {
   if (req.url.includes('i18n')) {
     return next(req);
   }
 
-  const cloneReq = req.clone({ url: `${environment.apiUrl}/${req.url}` });
+  if (req.url.startsWith('https://') || req.url.startsWith('http://')) {
+    return next(req);
+  }
+
+  let requestURL = req.url;
+  let apiURL = environment.apiUrl;
+  if (!requestURL.startsWith(apiURL)) {
+    requestURL = `${apiURL}/${req.url}`;
+  }
+  const cloneReq = req.clone({
+    url: requestURL,
+    headers: addIvyHeaders(req.headers)
+  });
   return next(cloneReq);
 };
+
+function addIvyHeaders(headers: HttpHeaders): HttpHeaders {
+  if (headers.has(REQUEST_BY)) {
+    return headers;
+  }
+  return headers.append(REQUEST_BY, IVY);
+}
