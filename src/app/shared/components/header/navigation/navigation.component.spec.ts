5ab3c3e8e4fd783da01623afe636a2f6ec209669
Dinh Nguyen <dinh.nguyentrinhquoc@axonactive.com>
2024-06-18 16:36:24 +0700

    fix gui test

--- src/app/shared/components/header/navigation/navigation.component.spec.ts
+++ src/app/shared/components/header/navigation/navigation.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
+import { Viewport } from 'karma-viewport/dist/adapter/viewport';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { NavigationComponent } from './navigation.component';
+declare const viewport: Viewport;
 
 describe('NavigationComponent', () => {
   let component: NavigationComponent;
@@ -8,10 +11,10 @@ describe('NavigationComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NavigationComponent]
-    })
-    .compileComponents();
-    
+      imports: [NavigationComponent, TranslateModule.forRoot()],
+      providers: [TranslateService]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(NavigationComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +22,27 @@ describe('NavigationComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('mobile search should display in small screen', () => {
+    viewport.set(540);
+
+    const mobileSearch = fixture.debugElement.query(
+      By.css('.header-mobile__search')
+    );
+
+    expect(getComputedStyle(mobileSearch.nativeElement).display).not.toBe(
+      'none'
+    );
+  });
+
+  it('mobile search should not display in large screen', () => {
+    viewport.set(1920);
+
+    const mobileSearch = fixture.debugElement.query(
+      By.css('.header-mobile__search')
+    );
+
+    expect(getComputedStyle(mobileSearch.nativeElement).display).toBe('none');
   });
 });
